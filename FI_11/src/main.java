c2fc32d219ff9e4854f0125e359710e0cae2c9b3
Bojana <38317008+BojanaVasilevska@users.noreply.github.com>
2023-04-02 18:40:18 +0200

    New feature

--- FI_11/src/main.java
+++ FI_11/src/main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
 
+        if (n < 0) {
+            System.out.println("Factorial is not defined for negative numbers");
+            return;
+        }
+
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+            if (factorial < 0) {
+                System.out.println("Overflow occurred!");
+                return;
+            }
+        }
+        System.out.println(n + "! = " + factorial);
 	}
 
 }
